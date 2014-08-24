95b5c3bed14ba3e4b83135b83035c960005cadc2
ads <adam@cangjie.info>
2014-08-24 00:22:44 -0400

    Added function to include all.php for retrieving bibliographic data from Zotero. If only a few pages use this, perhaps it should be included more selctively. At the moment, every page has access to the getZot function.

--- includes/all.php
+++ includes/all.php
@@ -7,6 +7,36 @@
 error_reporting(E_ALL);
 ini_set( 'display_errors','1');
 
+//ZOTERO getZot 
+//$from = 'keys' or 'tags', $keysOrTags is an array
+//returns full html bibliography, and coins data.
+function getZot($from='keys', $keysOrTags)
+{
+    $zot_style = 'elsevier-harvard2';
+    $zot_url_base = 'https://api.zotero.org/groups/280824/';
+    $zot_bib_url = $zot_url_base . '/items/?v=3&format=bib&style=' . $zot_style; 
+    $zot_coins_url = $zot_url_base . '/items/?v=3&format=coins';
+    if($from=='keys')
+    {
+        $suffix = '&itemKey=';
+        $glue = ',';
+    }
+    else if ($from=='tags')
+    {
+        $suffix = '&tag=';
+        $glue = '&tag=';
+    }
+    else return '';
+    $suffix .= implode($glue, $keysOrTags);
+    $zot_bib_url .= $suffix;
+    $zot_coins_url .= $suffix;
+    $zot_data = file_get_contents($zot_bib_url);
+    /* remove<?xml version="1.0"?> */
+    $zot_data = str_replace('<?xml version="1.0"?>', '', $zot_data);
+    $zot_data .= "\n" . file_get_contents($zot_coins_url);
+    return $zot_data;
+}
+
 //code to guess whether we are remote or local
 //and set paths accordingly
 if($_SERVER['DOCUMENT_ROOT']=='/var/www/html') //if local linux
